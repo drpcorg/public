3dd0120ab516f6ac115d1e411a47e058cafc14aa
Anna Linnik <annielinnik@gmail.com>
2024-09-10 13:31:10 +0400

    Add icons and descriptions (#29)
    * Add icons and descriptions
    
    * Add MetalL2 icon and description
    
    * Add CODEOWNERS file
    
    * Update README.md

---
+++ icons/getChainIcon.ts
@@ -0,0 +1,180 @@
+import Arbitrum from "./Arbitrum.svg";
+import ArbitrumNova from "./ArbitrumNova.svg";
+import ArbitrumNovaDark from "./ArbitrumNovaDark.svg";
+import Aurora from "./Aurora.svg";
+import Avalanche from "./Avalanche.svg";
+import AlephZero from "./AlephZero.svg";
+import Base from "./Base.svg";
+import BaseDark from "./BaseDark.svg";
+import Blast from "./Blast.svg";
+import BSC from "./BSC.svg";
+import Celo from "./Celo.svg";
+import CeloDark from "./CeloDark.svg";
+import Ethereum from "./Ethereum.svg";
+import Fantom from "./Fantom.svg";
+import FantomDark from "./FantomDark.svg";
+import Gnosis from "./Gnosis.svg";
+import Klaytn from "./Klaytn.svg";
+import Kusama from "./Kusama.svg";
+import Linea from "./Linea.svg";
+import Mantle from "./Mantle.svg";
+import MantleDark from "./MantleDark.svg";
+import Metis from "./Metis.svg";
+import Moonbeam from "./Moonbeam.svg";
+import OpBNB from "./OpBNB.svg";
+import Optimism from "./Optimism.svg";
+import Polkadot from "./Polkadot.svg";
+import Polygon from "./Polygon.svg";
+import Pyth from "./Pyth.svg";
+import Quicksilver from "./Quicksilver.svg";
+import Scroll from "./Scroll.svg";
+import Starknet from "./Starknet.svg";
+import Threshold from "./Threshold.svg";
+import Umnee from "./Umnee.svg";
+import Vega from "./Vega.svg";
+import Zksync from "./Zksync.svg";
+import Cronos from "./Cronos.svg";
+import Bitcoin from "./Bitcoin.svg";
+import Kava from "./Kava.svg";
+import ImmutableZkEvm from "./ImmutableZkEvm.svg";
+import Astar from "./Astar.png";
+import Solana from "./Solana.svg";
+import Vara from "./Vara.svg";
+import OKTChain from "./OKTChain.png";
+import Filecoin from "./Filecoin.svg";
+import Harmony from "./Harmony.svg";
+import Haqq from "./Haqq.svg";
+import Manta from "./Manta.png";
+import Taiko from "./Taiko.jpg";
+import Fuse from "./Fuse.png";
+import Berachain from "./Berachain.svg";
+import Lisk from "./Lisk.png";
+import Bittorrent from "./Bittorrent.png";
+import Boba from "./Boba.svg";
+import Core from "./Core.png";
+import Dymension from "./Dymension.png";
+import Evmos from "./Evmos.svg";
+import Heco from "./Heco.jpeg";
+import Mode from "./Mode.svg";
+import Near from "./Near.svg";
+import Neon from "./Neon.jpeg";
+import Playnance from "./Playnance.jpeg";
+import Ronin from "./Ronin.png";
+import Telos from "./Telos.png";
+import Thundercore from "./Thundercore.png";
+import Zetachain from "./Zetachain.png";
+import Zora from "./Zora.png";
+import Axelar from "./Axelar.svg";
+import Everclear from "./Everclear.png";
+import CosmosHub from "./CosmosHub.svg";
+import Neutron from "./Neutron.jpeg";
+import Osmosis from "./Osmosis.svg";
+import ReAl from "./ReAl.svg";
+import Rootstock from "./Rootstock.svg";
+import OpenCampusCodex from "./OpenCampusCodex.svg";
+import GameSwift from "./GameSwift.png";
+import Zircuit from "./Zircuit.svg";
+import Tron from "./Tron.png";
+import Fraxtal from "./Fraxtal.svg";
+import Goat from "./Goat.svg";
+import Metall2 from "./Metall2.svg";
+
+const Icons: Record<string, any> = {
+  arbitrum: Arbitrum,
+  "arb-blueberry": Arbitrum,
+  "arbitrum-nova": ArbitrumNova,
+  alephzero: AlephZero,
+  axelar: Axelar,
+  avalanche: Avalanche,
+  aurora: Aurora,
+  bitcoin: Bitcoin,
+  base: Base,
+  bsc: BSC,
+  blast: Blast,
+  celo: Celo,
+  gnosis: Gnosis,
+  fantom: Fantom,
+  polygon: Polygon,
+  "polygon-blackberry": Polygon,
+  "polygon-zkevm": Polygon,
+  "opcelestia-raspberry": Optimism,
+  optimism: Optimism,
+  linea: Linea,
+  mantle: Mantle,
+  metis: Metis,
+  moonbeam: Moonbeam,
+  moonriver: Moonbeam,
+  klaytn: Klaytn,
+  kusama: Kusama,
+  opbnb: OpBNB,
+  polkadot: Polkadot,
+  pyth: Pyth,
+  quicksilver: Quicksilver,
+  scroll: Scroll,
+  starknet: Starknet,
+  threshold: Threshold,
+  umnee: Umnee,
+  vega: Vega,
+  zksync: Zksync,
+  cronos: Cronos,
+  kava: Kava,
+  "immutable-zkevm": ImmutableZkEvm,
+  astar: Astar,
+  solana: Solana,
+  vara: Vara,
+  okt: OKTChain,
+  filecoin: Filecoin,
+  harmony: Harmony,
+  haqq: Haqq,
+  manta: Manta,
+  taiko: Taiko,
+  fuse: Fuse,
+  berachain: Berachain,
+  lisk: Lisk,
+  bittorrent: Bittorrent,
+  "boba-ethereum": Boba,
+  "boba-bnb": Boba,
+  core: Core,
+  dymension: Dymension,
+  evmos: Evmos,
+  heco: Heco,
+  mode: Mode,
+  near: Near,
+  neon: Neon,
+  playnance: Playnance,
+  ronin: Ronin,
+  everclear: Everclear,
+  "cosmos-hub": CosmosHub,
+  neutron: Neutron,
+  osmosis: Osmosis,
+  real: ReAl,
+  rootstock: Rootstock,
+  "open-campus-codex": OpenCampusCodex,
+  gameswift: GameSwift,
+  zircuit: Zircuit,
+  tron: Tron,
+  fraxtal: Fraxtal,
+  goat: Goat,
+  ethereum: Ethereum,
+  telos: Telos,
+  thundercore: Thundercore,
+  "zeta-chain": Zetachain,
+  zora: Zora,
+  metall2: Metall2,
+};
+
+const DarkIcons: Record<string, any> = {
+  "arbitrum-nova": ArbitrumNovaDark,
+  base: BaseDark,
+  celo: CeloDark,
+  fantom: FantomDark,
+  mantle: MantleDark,
+};
+
+export const getChainIcon = (network: string, isDark = false) => {
+  if (isDark) {
+    return DarkIcons[network] ?? Icons[network] ?? Ethereum;
+  }
+
+  return Icons[network] ?? Ethereum;
+};
