a8be0e653955db2111e66749b9aa9988b5be31f3
Vadim Filin <filinvadim@protonmail.com>
2025-12-29 18:08:59 +0800

    Add embed (#174)

---
+++ embed.go
@@ -0,0 +1,22 @@
+package emerald_grpc
+
+import _ "embed"
+
+//go:embed chains.yaml
+var chainConfig []byte
+
+//go:embed chains-meta.yaml
+var chainMeta []byte
+
+//go:embed compatible-clients.yaml
+var compatibleClients []byte
+
+func GetChainConfig() []byte {
+	return chainConfig
+}
+func GetChainMeta() []byte {
+	return chainConfig
+}
+func GetCompatibleClients() []byte {
+	return chainConfig
+}
